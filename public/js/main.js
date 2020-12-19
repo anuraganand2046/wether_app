b3c5d59f51b6dd423757e5712556fcc999f68cab
Anurag Anand <anuraganand@Anurags-MacBook-Air.local>
2020-12-19 16:24:02 +0530

    first commit

---
+++ public/js/main.js
@@ -0,0 +1,43 @@
+const submitbtn= document.getElementById("submitbtn");
+const cityName= document.getElementById("cityName");
+const city_name= document.getElementById("city_name");
+const temp_status= document.getElementById("temp_status");
+const temp= document.getElementById("temp");
+const datahide= document.querySelector('.middle_layer');
+const getInfo = async(event)=>{
+    event.preventDefault();
+    let val= cityName.value;
+    if(val==""){
+        city_name.innerText= `Please write the name of the city`;
+        datahide.classList.add('data_hide');
+    }
+    else{
+        try{
+            let url= `https://api.openweathermap.org/data/2.5/weather?q=${val}&appid=7f1c00d1ddbacaf37acc52f377913767`;
+            const response= await fetch(url);
+            const data= await response.json();
+            const arr= [data];
+            city_name.innerText= ` ${arr[0].name}, ${arr[0].sys.country}`;
+            temp.innerText= arr[0].main.temp;
+            const tempMood= arr[0].weather[0].main;
+            if(tempMood== "Clear"){
+                temp_status.innerHTML= "<i class='fa fa-sun' style= 'color: #eccc68;'></i>";
+            }
+            else if(tempMood== "Clouds"){
+                temp_status.innerHTML= "<i class='fa fa-cloud' style= 'color: #f1f2f6;'></i>";
+            }
+            else if(tempMood== "Rain"){
+                temp_status.innerHTML= "<i class='fa fa-rain' style= 'color: #a4b0be;'></i>";
+            }
+            else{
+                temp_status.innerHTML= "<i class='fa fa-cloud' style= 'color: #f1f2f6;'></i>";
+            }
+            datahide.classList.remove('data_hide');
+        }
+        catch{
+            city_name.innerText= `Please enter a proper city name.`;
+            datahide.classList.add('data_hide');
+        }
+    }
+}
+submitbtn.addEventListener('click', getInfo);
---
+++ src/app.js
@@ -0,0 +1,29 @@
+const express= require("express");
+const app= express();
+const path= require("path");
+const hbs= require("hbs");
+const port= 8000;
+const staticpath= path.join(__dirname, "../public");
+const templatepath= path.join(__dirname, "../templates/views");
+const partialspath= path.join(__dirname, "../templates/partials");
+app.set("view engine", "hbs");
+app.set("views", templatepath);
+app.use(express.static(staticpath));
+hbs.registerPartials(partialspath);
+app.get("/", (req, res)=>{
+    res.render("index");
+})
+app.get("/about", (req, res)=>{
+    res.render("about");
+})
+app.get("/weather", (req, res)=>{
+    res.render("weather");
+})
+app.get("*", (req, res)=>{
+    res.render("404", {
+        errormsg: "Oops!! Page not found.."
+    });
+})
+app.listen(port, ()=>{
+    console.log(`Listening to port ${port}`);
+})
